26d07b233eeabc39d6a6121989b26caaa1a72511
z1mEk <gabriel.zima@gmail.com>
2018-10-31 12:41:34 +0100

    fix

--- example.py
+++ example.py
@@ -1,7 +1,7 @@
 import sys
 from eSterownik35 import eSterownik35
 
-eStr35 = eSterownik35(IP='87.251.234.136', port='10121')
+eStr35 = eSterownik35(IP='192.168.1.50', port='80')
 
 device = int(sys.argv[1])
 value = sys.argv[2]
